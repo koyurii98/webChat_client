ffe5107a97879b587e51c72a9d23e3ef9a6032df
koyuri <rmeodprp0856@naver.com>
2021-08-18 15:38:57 +0900

    client 수정

--- src/Chat.js
+++ src/Chat.js
@@ -1,52 +1,102 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import io from 'socket.io-client';
+import moment from 'moment';
+import { useHistory } from 'react-router-dom';
 let socket ;
-const Chat = () => {
-  const [ name, setName ] = useState('');
+const Chat = (props) => {
+  const history = useHistory();
   const [ message, setMessage ] = useState('');
   const [ msgs , setMsgs] = useState([]);
-
+  const [ onLine, setOnLine ] = useState(false);
+  const queryString = props.location.search;
+  const params = new URLSearchParams(queryString);
+  const id = params.get("id");
+  const user_id = params.get("user_id");
+  const name = params.get("name");
+  const to = params.get("to");
 
   useEffect(()=>{
-    socket = io('http://localhost:8000');
-    socket.on('connect', ()=>{
+    if( !id && !user_id && !name && !to){
+      history.replace('/');
+    }
+  },[id, user_id, name, to])
+
+  useEffect(()=>{
+    socket = io(`http://localhost:8000?chatID=${user_id}&name=${user_id}`);
+    socket.on(`connect`, ()=>{
       console.log('서버와 연결됨');
-      if(!name){
-        let n = prompt('이름입력','');
-        socket.emit('newUser', n);
-        setName(n);
-      }
+      socket.emit('newUser', name);
     });
   },[]);
 
   useEffect(()=>{
-    socket.on('update', (data)=>{
-      console.log(`${data.name}:${data.message}`);
-      setMsgs(msgs.concat({name:data.name, msg: data.message}));
+    if(!onLine){
+      socket.on('update', ({name, message, time, type})=>{
+        if(type==='connect'){
+          setOnLine(true);
+          const msg = {
+            content:message,
+            senderChatID:name,
+            receiverChatID:user_id,
+            time:moment(time).format('hh:mm'),
+          }
+          setMsgs(msgs.concat(msg));
+        }else{
+          setOnLine(false)
+        }
+      });
+    }
+
+    socket.on('receive_message', ({message})=>{
+      const msg = {
+        content:message.content,
+        senderChatID:message.senderChatID,
+        receiverChatID:message.receiverChatID,
+        time:message.time
+      }
+      if(message.senderChatID == to){
+        setMsgs(msgs.concat(msg));
+      }
     });
-  },[])
+    
+  },[msgs,to, user_id])
 
   const send = useCallback(() => {
-    socket.emit('message', { type:'message', message: message});
-    setMsgs(msgs.concat({ name: name, msg: message }));
-  },[msgs, message]);
+    socket.emit('send_message', { type:'message', receiverChatID: to, senderChatID: user_id, content: message, time:moment().format('hh:mm')});
+    const msg = {
+      content:message,
+      senderChatID:user_id,
+      receiverChatID:to,
+      time:moment().format('hh:mm'),
+    }
+    setMsgs(msgs.concat(msg));
+  },[message, msgs]);
 
-  console.log(msgs);
+  
 
   return(
-    <div>
-      {
-        msgs.map((msg,i)=>{
-          return (
-            <div key={i}>
-              <span>{msg.name} - </span>
-              <span>{msg.msg}</span>
-            </div>
-            )
-        })
-      }
+    <div >
+      <span>{user_id}</span>
+      <div style={{ display:'flex', flexDirection:'row', alignItems:'center'}}>
+        <span>{to}</span><div style={{ borderRadius:"50%", width:"10px", height:"10px", backgroundColor:onLine?"green":"gray", marginLeft:"10px"}}></div>
+      </div>
+      <p>{to}님의 채팅방에 입장하셨습니다.</p>
+      <div style={{display:'flex', flexDirection:'column', width:'400px'}}>
+        {
+          msgs.map((msg,i)=>{
+            return (
+              <div key={i} style={{ color: msg.senderChatID == user_id ? 'red': 'black'}}>
+                <span>{msg.senderChatID} - </span>
+                <span>{msg.content}</span>
+                <span>{msg.time}</span>
+              </div>
+              )
+          })
+        }
+      </div>
       <input type="text" onChange={(e)=>setMessage(e.target.value)}/>
-      <button onClick={()=>send()}>전송</button>
+      <button onClick={()=>send()} onkeydown={()=>{}}>전송</button>
+      <span onClick={()=>setMsgs([])}>채팅내용지우기</span>
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from 'react';
-import io from 'socket.io-client';
-let socket ;
+import React, { useEffect, useMemo } from 'react';
+import { useHistory } from 'react-router-dom';
+
 const Main = () => {
+  const history = useHistory();
 
   return(
-    <div>화면</div>
+    <div>
+      <button onClick={()=>history.push('/chat?id=1&user_id=yul&name=yul&to=ri')}>채팅방1입장</button>
+      <button onClick={()=>history.push('/chat?id=2&user_id=ri&name=ri&to=yul')}>채팅방2입장</button>
+      <button onClick={()=>history.push('/chat?id=3&user_id=jun&name=jun&to=yul')}>채팅방3입장</button>
+    </div>
   );
 }
 
